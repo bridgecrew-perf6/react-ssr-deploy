6b5122b97f4228de61bc300966e9df8ca678c1c5
dalideco <59741273+dalideco@users.noreply.github.com>
2022-02-10 14:57:02 +0100

    update application start for new ec2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,14 +6,19 @@ sudo chmod -R 777 /home/ec2-user/express-app
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/express-app
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
 #install node modules
+echo "<--installing dependendices"
 npm install
+echo "installed dependencies-->"
+
+echo "<--building application"
 npm run build
+echo "built application-->"
 
 #start our node app in the background
-node dist/main.js > app.out.log 2> app.err.log < /dev/null & 
+echo "<--starting app"
+echo "starting"
+sudo systemctl start NodeServer.service
+echo "enabling"
+sudo systemctl enable NodeServer.service
+echo "started app -->"
