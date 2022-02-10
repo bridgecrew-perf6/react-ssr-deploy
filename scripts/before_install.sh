3d8421742feae0c488bf0cd956a5e91a46456c72
dalideco <dalissj1@gmail.com>
2022-02-10 13:14:01 +0100

    removing files and killing port 3000 at application stop

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16
 
+# deleting directory and gitignore file
+echo "deleting"
+rm -rf /home/ec2-user/express-app/.gitignore
+rm -rf /home/ec2-user/express-app
+echo "deleted"
+
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
 if [ -d "$DIR" ]; then
