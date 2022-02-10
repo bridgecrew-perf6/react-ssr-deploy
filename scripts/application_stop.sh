39d21a456ceab66144547c557f3dd24db431385d
dalideco <59741273+dalideco@users.noreply.github.com>
2022-02-10 14:50:16 +0100

    updating application stop for another ec2

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers"
-echo "test test"
+echo "<-- Stopping any existing node servers"
 
+echo "stopping node server"
+sudo systemctl stop NodeServer.service
+echo "disabling node server"
+sudo systemctl disable NodeServer.service
+echo "kiling anything in the port"
 kill -9 $(lsof -t -i:3000)
-# pkill node
-#testing if ec2 receives this
+
+echo "Stopped existing node server -->
