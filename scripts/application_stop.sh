5a4c69de879832b87aa8438513658d2ae5ad58b0
dalideco <dalissj1@gmail.com>
2022-02-10 12:51:23 +0100

    removing files and killing port 3000 at application stop

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,5 +2,7 @@
 #Stopping existing node servers
 echo "Stopping any existing node servers"
 echo "test test"
+rm -rf /home/ec2-user/express-app
+kill -9 $(lsof -t -i:3000)
 # pkill node
 #testing if ec2 receives this
